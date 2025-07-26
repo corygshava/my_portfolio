561075b13afa7a528ced0d74611fcaca070095a1
corygshava <corygshava@gmail.com>
2025-07-26 15:11:44 +0300

    latest changes (badges plus working project links)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // let links = undefined;
 let heightbox = undefined;
 // let navbar = undefined;
+let myskills = [];
 
 window.addEventListener('scroll', e => {
     // change up the navbar
@@ -32,24 +33,33 @@ window.addEventListener('scroll', e => {
         let links = el.querySelectorAll('a');
 
         links.forEach(lnk => {
-            let thelink = lnk.dataset.mylink.split("#")[1];
+            if(lnk.dataset.mylink != undefined){
+                let thelink = lnk.dataset.mylink.split("#")[1];
 
-            if (thelink == `${theid}`) {
-                lnk.classList.add('active');
-                // lnk.classList.add('w3-white');
-            } else {
-                lnk.classList.remove('active');
-                // lnk.classList.remove('w3-white');
+                if (thelink == `${theid}`) {
+                    lnk.classList.add('active');
+                    // lnk.classList.add('w3-white');
+                } else {
+                    lnk.classList.remove('active');
+                    // lnk.classList.remove('w3-white');
+                }
+                console.log(`checking link ${theid} vs ${thelink}`);
             }
-            console.log(`checking link ${theid} vs ${thelink}`);
         })
     })
 })
 
 window.addEventListener('load', e => {
+    renderSkillz('#skills_main',"main");
+    renderSkillz('#skills_sec',"sec");
+
     mekExtras();
     mekVars();
     mekEvents();
+
+    renderProjects('#doneprojos','',true,3);
+    renderProjects('#ongoingprojos','',false,3);
+    showprods('#tehtable');
 })
 
 // functions
@@ -67,7 +77,25 @@ function mekExtras() {
 }
 
 function mekVars() {
-    // UI variables
+    myskills = [
+        {
+            category : "myprojects", 
+            title: "uploader premium",
+            link: "http://haoselkenya.com/pjs/cfw/uploader-premium/",
+            img: "images/project_1.png",
+            desc: "an upload manager i made to hold my editing projects plus as an online backup for som e of my files",
+            tech: "html,css,js,php,json",
+            client: "personal project"
+        },{
+            category : "myprojects", 
+            title: "Foodacious",
+            link: "http://haoselkenya.com/pjs/cfw/uploader-premium/",
+            img: "images/project_5.png",
+            desc: "a wordpress e-commerce site i made to figure out selling and buying stuff online",
+            tech: "html,css,js,php,json,wordpress",
+            client: "personal project"
+        }
+    ]
 }
 
 function mekEvents() {
@@ -88,4 +116,113 @@ function mekEvents() {
             phonemenu.classList.add("showme");
         }
     })
+
+    let skills = document.querySelectorAll('.skill');
+
+    skills.forEach(el => {
+        el.addEventListener('click',e => {
+            toggleShowB('#infomodal','flex','none');
+
+            let hed = infomodal.querySelector('h1');
+            let par = infomodal.querySelector('p');
+
+            hed.innerHTML = el.querySelector('.subtxt').innerHTML;
+            par.innerHTML = el.querySelector('.mydata').innerHTML;
+        })
+    })
+}
+
+function renderProjects(where,type,isdone,limit) {
+    let thebox = document.querySelector(where);
+
+    if(thebox != null){
+        thebox.innerHTML = '';
+        let mylist = [];
+        let count = 0;
+
+        projects.forEach(el => {
+            if(el.isdone == isdone && count < limit){
+                mylist.push(el);
+                count += 1;
+            } else {
+                console.log("not done");
+            }
+        });
+
+        mylist.forEach(el => {
+            thebox.innerHTML += `
+                <div class="project">
+                    <img src="images/${el.img}" alt="${el.title} image" class="theimg">
+                    <div class="caption">
+                        <div class="pname">${el.title}</div>
+                        <a target="_blank" href="${el.link}" class="themebtn">visit</a>
+                    </div>
+                </div>
+            `;
+        })
+    }
+}
+
+function showprods(where) {
+    let thebox = document.querySelector(where);
+
+    if(thebox != null){
+        let html = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>title</th>
+                        <th>category</th>
+                        <th>link</th>
+                    </tr>
+                </thead>
+        `;
+
+        projects.forEach(el => {
+            let ver = el.isdone ? "done" : "prototype";
+            html += `
+                <tr>
+                    <td>${el.title}</td>
+                    <td><b class="w3-tag w3-orange">${el.cat}</b></td>
+                    <td><a target="_blank" href="${el.link}" class="themebtn">visit</a></td>
+                </tr>
+            `;
+        })
+
+        html += `
+            </table>
+        `;
+
+        thebox.innerHTML = html;
+    } else {
+        alert('display missing')
+    }
+}
+
+function renderSkillz(where,cat) {
+    let showbox = document.querySelector(where);
+    let touse = [];
+
+    skills.forEach(el => {
+        if(el.cat == cat){
+            touse.push(el);
+        }
+    })
+
+    if(showbox != undefined){
+        showbox.innerHTML = ``;
+
+        touse.forEach(el => {
+            showbox.innerHTML += `
+                <div class="skill">
+                    <div class="skilllvl">${el.level}</div>
+                    <img src="images/${el.img}" alt="${el.skillname} icon">
+                    <span class="subtxt">${el.skillname}</span>
+                    <div class="mydata w3-hide">
+                        ${el.desc}
+                    </div>
+                </div>
+            `;
+        })
+    }
 }
---
+++ js/datas.js
@@ -0,0 +1,131 @@
+let projects = [
+	{
+		img : 'project_3.png',
+		title: 'Magnus Consultancy',
+		link: 'http://haoselkenya.com/pjs/cfw/consultancy_website/',
+		cat: 'personal',
+		isdone: true
+	},
+	/*{
+		img : 'project_6.png',
+		title: 'Ziga E-commerce',
+		link: 'http://haoselkenya.com/pjs/cfw/ecommerce/',
+		cat: 'personal',
+		isdone: true
+	},
+	{
+		img : 'project_5.png',
+		title: 'Foodacious',
+		link: '/foodacious/',
+		cat: 'personal',
+		isdone: true
+	},*/
+	{
+		img : 'project_1.png',
+		title: 'Uploader Premium',
+		link: 'http://haoselkenya.com/pjs/cfw/uploader-premium/',
+		cat: 'personal',
+		isdone: true
+	},
+	{
+		img : 'project_1.png',
+		title: 'nima East Africa website',
+		link: 'http://nimaeastafrica.com/',
+		cat: 'complete',
+		isdone: true
+	},
+	{
+		img : 'project_1.png',
+		title: 'Diamond engineering Kenya website redesign',
+		link: 'http://diamondeng.co.ke/newdesign/',
+		cat: 'complete',
+		isdone: true
+	},
+	{
+		img : 'project_1.png',
+		title: 'Haosel Kenya website',
+		link: 'http://haoselkenya.com/',
+		cat: 'complete',
+		isdone: true
+	},
+	{
+		img : 'project_2.png',
+		title: 'Thamani finance web app',
+		link: 'https://v0-thamani-web-app.vercel.app/',
+		cat: 'prototype',
+		isdone: true
+	},
+	{
+		img : 'project_2.png',
+		title: 'Upload manager prototype',
+		link: 'https://v0-simple-upload-manager.vercel.app/',
+		cat: 'prototype',
+		isdone: false
+	}
+];
+
+let skills = [
+	{
+		level: "proficient",
+		skillname: "HTML",
+		cat: "main",
+		desc: "Provides the foundational structure of web pages by defining elements displayed on the page. It is essential for all web development and works in tandem with CSS and JavaScript to create functional and visually appealing websites.",
+		img: `skill_1.png`
+	},
+	{
+		level: "Expert",
+		skillname: "CSS",
+		cat: "main",
+		desc: "Enables developers to separate content from design, allowing for responsive, visually engaging user interfaces. Features like Flexbox, Grid, animations, and media queries empower modern web styling across devices.",
+		img: `skill_2.png`
+	},
+	{
+		level: "Proficient",
+		skillname: "Javascript",
+		cat: "main",
+		desc: "Brings interactivity to websites by manipulating the DOM, handling events, and enabling asynchronous communication (AJAX). It powers dynamic features like form validation, animations, and real-time updates, and is also widely used on the server-side via Node.js.",
+		img: `skill_3.jpg`
+	},
+	{
+		level: "Proficient",
+		skillname: "PHP",
+		cat: "main",
+		desc: "Used to generate dynamic page content, handle form submissions, manage sessions, and interact with databases. It integrates seamlessly with HTML and powers popular platforms like WordPress, making it a staple for backend web development.",
+		img: `skill_4.png`
+	},
+	{
+		level: "Proficient",
+		skillname: "SQL",
+		cat: "main",
+		desc: "Used to query, update, and manage data stored in database systems like MySQL, PostgreSQL, and SQL Server. It allows developers to retrieve insights, build reports, and maintain structured data efficiently.",
+		img: `skill_2.png`
+	},
+	{
+		level: "Intermediate",
+		skillname: "Node JS",
+		cat: "sec",
+		desc: "Enables developers to use JavaScript on the server side, allowing for full-stack development using a single language. It's particularly well-suited for building APIs, real-time applications, and microservices due to its non-blocking, event-driven architecture.",
+		img: `skill_6.png`
+	},
+	{
+		level: "Intermediate",
+		skillname: "React",
+		cat: "sec",
+		desc: "Developed and maintained by Facebook, it allows developers to build reusable UI components and manage complex application states efficiently using a virtual DOM. It is widely used for creating fast, interactive front-end experiences.",
+		img: `skill_7.png`
+	},
+	{
+		level: "Expert",
+		skillname: "System design",
+		cat: "sec",
+		desc: "Ensures that applications are scalable, maintainable, and performant. It involves understanding requirements, selecting appropriate technologies, designing databases, and planning interactions between different parts of a system to meet both current and future needs.",
+		img: `skill_8.png`
+	},
+	{
+		level: "Intermediate",
+		skillname: "Wordpress",
+		cat: "sec",
+		desc: "Provides a user-friendly interface for creating blogs, business sites, portfolios, and e-commerce platforms. It supports themes for design customization and plugins for extended functionality, making it a popular choice for both developers and non-technical users.",
+		img: `skill_9.png`
+	}
+]
